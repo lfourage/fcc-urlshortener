8dda0fcc2e4e084de63b1de1216f5dd29f870953
lfourage <ludogriph@gmail.com>
2025-06-25 16:43:24 +0200

    probable infinite loop

--- index.js
+++ index.js
@@ -2,10 +2,18 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const bodyParser = require("body-parser");
+const URL = require("url").URL;
+const dns = require("dns");
+const mongoose = require("mongoose");
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
-const mongo_uri = process.env.MONGP_URI;
 
 app.use(cors());
 
@@ -13,6 +21,54 @@ app.use("/public", express.static(`${process.cwd()}/public`));
 
 app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html");
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Database
+
+const urlSchema = new mongoose.Schema({
+  original_url: String,
+  short_url: Number,
+});
+
+const urlModel = mongoose.model("urlModel", urlSchema);
+
+const generateShortUrl = () => {
+  let shortURL = Math.floor(Math.random() * 10000);
+
+  while (urlModel.exists({ short_url: shortURL }))
+    shortURL = Math.floor(Math.random() * 10000);
+  return shortURL;
+};
+
+app.post("/api/shorturl/", (req, res) => {
+  const originalURL = req.body.url;
+  const shortURL = generateShortUrl();
+  let urlObject;
+
+  try {
+    urlObject = new URL(originalURL);
+  } catch (err) {
+    res.json({ error: "Invalid URL" });
+  }
+
+  dns.lookup(urlObject.hostname, (err) => {
+    if (err) res.json({ error: "Invalid Hostname" });
+
+    if (!urlModel.exists({ original_url: originalURL })) {
+      const newUrlModel = new urlModel({
+        original_url: originalURL,
+        short_url: shortURL,
+      });
+
+      newUrlModel.save((err, data) => {
+        if (err) console.error(err);
+        done(null, data);
+      });
+    }
+    res.json({ original_url: originalURL, short_url: shortURL });
+  });
 });
 
 // Your first API endpoint
